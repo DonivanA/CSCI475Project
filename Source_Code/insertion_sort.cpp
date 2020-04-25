487c445ecd0b13bd2f2fb0f5118730d30d5f93f9
DonivanA <64321719+DonivanA@users.noreply.github.com>
2020-04-25 11:10:39 -0500

    Create insertion_sort.cpp

---
+++ Source_Code/insertion_sort.cpp
@@ -0,0 +1,12 @@
+/*CSCI 475 
+insertion sort
+Donivan Anderson
+Brent Clapp*/
+
+/* Steps to take for insertion sort
+1. We need an array, select the pivot, divide into processes
+2. we separate the elements <= pivot into front of each processes
+3. move all elements <= pivot into front of entire array
+4. assign elements to processes (but we need to keep processes separate from last pivot)
+5. repeat to 1 until each process is sorted.
+*/
